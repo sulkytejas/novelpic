f08aebc8f6ccce0cb931e4cb5eed72a5fe6e58ca
tejas Pashte <sulkytejas@yahoo.co.in>
2017-09-06 09:25:26 -0700

    test

--- src/App.js
+++ src/App.js
@@ -13,16 +13,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="app">
-        <Route path="/" render ={()=>(
+        <Route exact path="/" render ={()=>(
           <BookShelf/>
         )}/>
-
       </div>
     )
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class BookShelf extends Component{
   render(){
@@ -159,7 +160,7 @@ class BookShelf extends Component{
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search" onClick={() => this.setState({ showSearchPage: true })}>Add a book</Link>
         </div>
       </div>
     )
